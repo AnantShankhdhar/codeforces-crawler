ac9d8cff34362a336debd6487936979a137a81d3
Divyam Singal <divyamsingal01@gmail.com>
2020-04-22 18:36:29 +0530

    Made changes

--- userinfo/views.py
+++ userinfo/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'userinfo/index.html' )
+
+def detail(request):
+    return render(request, 'userinfo/detail.html')
