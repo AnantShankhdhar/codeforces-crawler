c934bd4332204742afcae3059865c27863b98d7d
210manishprajapati <58730178+210manishprajapati@users.noreply.github.com>
2020-04-23 17:19:25 +0530

    user name displayed

--- userinfo/views.py
+++ userinfo/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 
+
 def index(request):
-    return render(request, 'userinfo/index.html' )
+    return render(request, 'userinfo/index.html')
+
 
 def detail(request):
-    return render(request, 'userinfo/detail.html')
+    user = request.POST['user']  # user is the name of the input
+    return render(request, 'userinfo/detail.html', {'user': user, })
+
+
