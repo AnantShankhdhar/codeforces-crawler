23694d0ae410888dd56815a9283c7a3096b1f809
210manishprajapati <58730178+210manishprajapati@users.noreply.github.com>
2020-04-24 20:49:26 +0530

    models updated

--- userinfo/models.py
+++ userinfo/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Submission(models.Model):
+    prob_level = models.CharField(max_length=1)
+    prob_rating = models.IntegerField()
+    prob_tags = models.BooleanField(default=False)
