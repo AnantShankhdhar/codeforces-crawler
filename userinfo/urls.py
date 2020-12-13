756347d592f5fcb15fb48dcfd7edaf6568f709a7
Manish Prajapati <210manishprajapati@gmail.com>
2020-12-13 18:14:49 +0530

    two user compares backend done

--- userinfo/urls.py
+++ userinfo/urls.py
@@ -7,6 +7,8 @@ app_name = 'userinfo'
 urlpatterns = [
     path('userinfo/',views.index, name='index'),
     path('userinfo/detail/',views.detail,name='detail'),
+    path('userinfo/Inputcompares/',views.inputCompares,name='inputCompares'),
+    path('userinfo/Inputcompares/compares/',views.Compares,name='Compares'),
     path('',views.signup,name='signup'),
     path('login/',auth_views.LoginView.as_view(template_name='userinfo/login.html'),name='login'),
     path('logout/',auth_views.LogoutView.as_view(template_name='userinfo/logout.html'),name='logout'),
--- userinfo/views.py
+++ userinfo/views.py
@@ -141,6 +141,228 @@ def detail(request):
                        }
                       )
 
+def inputCompares(request):
+    return render(request, 'userinfo/inputcompares.html')
+
+
+def Compares(request):
+    Firstuser = request.POST['Firstuser']
+    Seconduser = request.POST['Seconduser']
+    # rank,color,ar,institute,ac,wa,tle,rte,mle,challenged,cpe,skipped,ile,other = scrape(user)
+    Firstverdict = scrape(Firstuser)
+    Secondverdict = scrape(Seconduser)
+    if Firstverdict == False or Secondverdict==False:
+        Firstexists = Firstverdict
+        Secondexists = Secondverdict
+        return render(request, 'userinfo/compares.html',
+                    {
+                    'Secondexists': Secondexists,
+                    'Firstexists': Firstexists
+                    })
+
+    else:
+        # First User Verdict
+        Firstexists = Firstverdict[0]
+        Firstcontests_given = Firstverdict[1]
+        Firstname = Firstverdict[2]
+        Firstrating = Firstverdict[3]
+        Firstmaxrating = Firstverdict[4]
+        Firstcountry = Firstverdict[5]
+        Firstcity = Firstverdict[6]
+        Firstorganisation = Firstverdict[7]
+        Firstrank = Firstverdict[8]
+        Firstmaxrank = Firstverdict[9]
+
+        FirstTagList = Firstverdict[10]
+        FirstProbRatList = Firstverdict[11]
+        FirstTypeList = Firstverdict[12]
+        FirstLangList = Firstverdict[13]
+        FirstVerdictList = Firstverdict[14]
+
+        FirstcontestTime = Firstverdict[15]
+        Firstranks = Firstverdict[16]
+        FirstoldRatings = Firstverdict[17]
+        FirstnewRatings = Firstverdict[18]
+        FirstbestRank = Firstverdict[19]
+        FirstworstRank = Firstverdict[20]
+
+        # Second User Verdict
+
+        Secondexists = Secondverdict[0]
+        Secondcontests_given = Secondverdict[1]
+        Secondname = Secondverdict[2]
+        Secondrating = Secondverdict[3]
+        Secondmaxrating = Secondverdict[4]
+        Secondcountry = Secondverdict[5]
+        Secondcity = Secondverdict[6]
+        Secondorganisation = Secondverdict[7]
+        Secondrank = Secondverdict[8]
+        Secondmaxrank = Secondverdict[9]
+
+        SecondTagList = Secondverdict[10]
+        SecondProbRatList = Secondverdict[11]
+        SecondTypeList = Secondverdict[12]
+        SecondLangList = Secondverdict[13]
+        SecondVerdictList = Secondverdict[14]
+
+        SecondcontestTime = Secondverdict[15]
+        Secondranks = Secondverdict[16]
+        SecondoldRatings = Secondverdict[17]
+        SecondnewRatings = Secondverdict[18]
+        SecondbestRank = Secondverdict[19]
+        SecondworstRank = Secondverdict[20]
+
+        #First User verdicts changes
+
+        Firstac=FirstVerdictList['OK']
+
+        FirstVerdictList['Accepted'] = FirstVerdictList['OK']
+        del FirstVerdictList['OK']
+
+
+        FirstTypeList_label = []
+        FirstTypeList_data = []
+
+        for i in sorted (FirstTypeList) :
+            FirstTypeList_data.append(FirstTypeList[i])
+            FirstTypeList_label.append(i)
+
+        FirstLangList_label = []
+        FirstLangList_data = []
+
+        for i in sorted (FirstLangList) :
+            FirstLangList_data.append(FirstLangList[i])
+            FirstLangList_label.append(i)
+
+        FirstProbRatList_label = []
+        FirstProbRatList_data = []
+
+        for i in sorted (FirstProbRatList) :
+            FirstProbRatList_data.append(FirstProbRatList[i])
+            FirstProbRatList_label.append(i)
+
+        FirstVerdictList_label = []
+        FirstVerdictList_data = []
+
+        FirstVerdictList = dict([(value, key) for key, value in FirstVerdictList.items()])
+        for i in sorted (FirstVerdictList, reverse= True) :
+            FirstVerdictList_label.append(FirstVerdictList[i])
+            FirstVerdictList_data.append(i)
+
+        FirstTagList_label = []
+        FirstTagList_data = []
+
+        FirstTagList = dict([(value, key) for key, value in FirstTagList.items()])
+        for i in sorted (FirstTagList, reverse=True) :
+            FirstTagList_label.append(FirstTagList[i])
+            FirstTagList_data.append(i)
+
+        FirstcontestTimegood = []
+        for i in (FirstcontestTime):
+            t = time.strftime('%Y-%m-%d', time.localtime(i))
+            FirstcontestTimegood.append(t)
+
+        #Second User verdicts changes
+
+        Secondac=SecondVerdictList['OK']
+
+        SecondVerdictList['Accepted'] = SecondVerdictList['OK']
+        del SecondVerdictList['OK']
+
+
+        SecondTypeList_label = []
+        SecondTypeList_data = []
+
+        for i in sorted (SecondTypeList) :
+            SecondTypeList_data.append(SecondTypeList[i])
+            SecondTypeList_label.append(i)
+
+        SecondLangList_label = []
+        SecondLangList_data = []
+
+        for i in sorted (SecondLangList) :
+            SecondLangList_data.append(SecondLangList[i])
+            SecondLangList_label.append(i)
+
+        SecondProbRatList_label = []
+        SecondProbRatList_data = []
+
+        for i in sorted (SecondProbRatList) :
+            SecondProbRatList_data.append(SecondProbRatList[i])
+            SecondProbRatList_label.append(i)
+
+        SecondVerdictList_label = []
+        SecondVerdictList_data = []
+
+        SecondVerdictList = dict([(value, key) for key, value in SecondVerdictList.items()])
+        for i in sorted (SecondVerdictList, reverse= True) :
+            SecondVerdictList_label.append(SecondVerdictList[i])
+            SecondVerdictList_data.append(i)
+
+        SecondTagList_label = []
+        SecondTagList_data = []
+
+        SecondTagList = dict([(value, key) for key, value in SecondTagList.items()])
+        for i in sorted (SecondTagList, reverse=True) :
+            SecondTagList_label.append(SecondTagList[i])
+            SecondTagList_data.append(i)
+
+        SecondcontestTimegood = []
+        for i in (SecondcontestTime):
+            t = time.strftime('%Y-%m-%d', time.localtime(i))
+            SecondcontestTimegood.append(t)
+
+        #returning both user verdicts
+        return render(request, 'userinfo/compares.html',
+                      {
+                      'Secondexists': Secondexists,
+                      'Secondcontests_given': Secondcontests_given,
+                      'Secondname': Secondname,
+                      'Secondrating': Secondrating,
+                      'SecondcontestTime':SecondcontestTime,
+                      'SecondcontestTimegood':SecondcontestTimegood,
+                      'Secondranks':Secondranks,
+                      'SecondnewRatings':SecondnewRatings,
+                      'SecondbestRank': SecondbestRank,
+                      'SecondTagList_data':SecondTagList_data,
+                      'SecondTagList_label':SecondTagList_label,
+                      'SecondProbRatList_data':SecondProbRatList_data,
+                      'SecondProbRatList_label':SecondProbRatList_label,
+                      'SecondTypeList_data':SecondTypeList_data,
+                      'SecondTypeList_label':SecondTypeList_label,
+                      'SecondVerdictList_data':SecondVerdictList_data,
+                      'VerdictList_label':SecondVerdictList_label,
+                      'SecondLangList_data':SecondLangList_data,
+                      'SecondLangList_label':SecondLangList_label,
+                      'Firstexists': Firstexists,
+                      'Firstcontests_given': Firstcontests_given,
+                      'Firstname': Firstname,
+                      'Firstrating': Firstrating,
+                      'FirstcontestTime':FirstcontestTime,
+                      'FirstcontestTimegood':FirstcontestTimegood,
+                      'Firstranks':Firstranks,
+                      'FirstnewRatings':FirstnewRatings,
+                      'FirstbestRank': FirstbestRank,
+                      'FirstTagList_data':FirstTagList_data,
+                      'FirstTagList_label':FirstTagList_label,
+                      'FirstProbRatList_data':FirstProbRatList_data,
+                      'FirstProbRatList_label':FirstProbRatList_label,
+                      'FirstTypeList_data':FirstTypeList_data,
+                      'FirstTypeList_label':FirstTypeList_label,
+                      'FirstVerdictList_data':FirstVerdictList_data,
+                      'FirstVerdictList_label':FirstVerdictList_label,
+                      'FirstLangList_data':FirstLangList_data,
+                      'FirstLangList_label':FirstLangList_label
+                       }
+                      )
+
+
+
+
+
+
+
+
 def signup(request):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
