9799f6c68bd683f95b1e2a18cdcaa8ca0caf53f2
Divyam Singal <divyamsingal01@gmail.com>
2020-12-20 20:38:27 +0530

    Shortened url

--- codeforces_crawler/urls.py
+++ codeforces_crawler/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('userinfo/',include('userinfo.urls')),
+    path('',include('userinfo.urls')),
 ]
